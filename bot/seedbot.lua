2ce476ce42b42627d4bd0db08c10f81bdf7db8c7
kiarash <info@Kiava.ir>
2016-11-11 14:38:56 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -271,7 +271,7 @@ function create_config( )
    "writer2"
     
     },
-    sudo_users = {228552783,36030970,0,tonumber(our_id)},--Sudo users
+    sudo_users = {247134702,247134702,0,tonumber(our_id)},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[kiavaprotection v4
 An advanced administration bot based on TG-CLI written in Lua
